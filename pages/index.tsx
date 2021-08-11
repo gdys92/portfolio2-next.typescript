43fb2eb22697692084e7e8940acb28cb68ffc379
gdys92 <gdys92@gmail.com>
2021-08-11 10:13:52 -0300

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const About = () => {
 
          <h6 className='my-3 text-base font-medium'>
              I have 4+ years of
-            experience in Web Development
+            experience in Web Development.
          </h6>
          <div
             className='flex-grow p-4 mt-5 bg-gray-400 dark:bg-black-100'
