4c62a41f1d058dcbb851685642a8b891478ca6a6
gdys92 <gdys92@gmail.com>
2021-10-19 17:49:21 -0300

    data2

--- data.ts
+++ data.ts
@@ -24,7 +24,7 @@ const services: Service[] = [
       Icon: AiOutlineApi,
       title: 'API Development',
       about:
-         'I ca develop robust  REST API using <b>Node API</b> ',
+         'I can develop robust  REST API using <b>Node API</b> ',
    },
    {
       Icon: DiDatabase,
